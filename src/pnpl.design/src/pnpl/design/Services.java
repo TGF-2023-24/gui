9972fa946e1d3313f585c2b96a47810497ea5efc
eduher04 <eduher04@ucm.es>
2024-09-01 11:33:41 +0200

    FM validation rules

--- src/pnpl.design/src/pnpl/design/Services.java
+++ src/pnpl.design/src/pnpl/design/Services.java
@@ -17,26 +17,4 @@ public class Services {
 	      return self;
 	    }
 	 
-	 public void deleteRelationIfOnlyChild(EObject element) {
-	        // Obtén el contenedor del nodo, asumiendo que es una relación o que contiene relaciones
-	        EObject container = element.eContainer();
-
-	        if (container == null) {
-	            return; // Si el contenedor es nulo, no se puede proceder
-	        }
-
-	        // Obtén las relaciones desde el contenedor (modifica 'getRelations' y 'getChilds' según tu metamodelo)
-	        List<EObject> relations = (List<EObject>) container.eGet(container.eClass().getEStructuralFeature("relations"));
-
-	        for (EObject relation : relations) {
-	            List<EObject> children = (List<EObject>) relation.eGet(relation.eClass().getEStructuralFeature("Childs"));
-
-	            // Verifica si la relación tiene al nodo como único hijo
-	            if (children.contains(element) && children.size() == 1) {
-	                // Elimina la relación
-	                EcoreUtil.delete(relation);
-	                break; // Sale del bucle después de eliminar la relación
-	            }
-	        }
-	 }
 }
